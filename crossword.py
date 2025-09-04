004234a69764e1fa554bc882dea674b7df70bedc
Maddie O'Brien <maddieobrien@Maddies-MacBook-Air.local>
2025-09-04 13:39:01 -0400

    Initial commit with crossword.py and wordlist.txt

---
+++ crossword.py
@@ -0,0 +1,33 @@
+"""
+crossword.py
+Crossword Puzzle Generator
+Author: Madison Taylor
+Date: September 2025
+
+Description: 
+
+==================================================================="""
+# ------------------------------------------
+# IMPORTS
+# ------------------------------------------
+
+import 
+
+# ------------------------------------------
+# FUNCTION DEFINITIONS
+# ------------------------------------------
+
+def 
+
+
+# ------------------------------------------
+# MAIN CODE
+# ------------------------------------------
+
+
+
+## Greeting
+
+print("Hello! This is the start of my crossword project.")
+
+## Generate Crossword
